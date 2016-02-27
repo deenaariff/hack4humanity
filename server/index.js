10e4b6cdee883aac9a01e452cdf21b8908577464
daariff <aariff.deen@gmail.com>
2016-02-27 11:55:14 -0800

    initial server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 var app = require('express');
+var PORT = 3000;
+
+// Modules 
+var users = 
+var events = 
+var requesterAPI = require('');
+var workerAPI = require('');
+var dashboard API = require('');
+
+app.listen(PORT);
