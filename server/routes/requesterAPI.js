8e147419d05c4783801b535a4ad1eb29a774050b
daariff <aariff.deen@gmail.com>
2016-02-27 15:29:46 -0800

    updated depdencies and modules

--- server/routes/requesterAPI.js
+++ server/routes/requesterAPI.js
@@ -1,20 +1,42 @@
 module.exports = function (app db, ObjectID) {
 
-  var ObjectId = ObjectID;
+ 	var ObjectId = ObjectID;
 
-  app.get('/allRequesters', function (req, res) { {
-          db.collection('users').find(function(err,docs) {
-              var jsonStr = JSON.stringify(docs);
-              res.end(jsonStr);
-              if (err) {
-                res.writeHead(403, {
-                    'Content-Type': 'application/json; charset=utf-8'
-                });
-                res.end(JSON.stringify ({
-                    error: "DB Error"
-                }));
-              }
+  	app.put('/requester/workersNeeded/:id/:num', function (req, res, next) {
+
+        db.events.findOne({
+          _id: db.ObjectId(req.params.id),
+        }, function (err, data) {
+       	  if(!data)
+       	  	res.send("Param did not work")
+
+          data.workers_needed = req.params.num;
+
+          db.foodList.update({
+            _id: db.ObjectID(req.params.id)
+          }, data, {
+            multi: false
+          }, function (err, data) {
+            if (err)
+            	res.send("ERROR: Updating database failed")
           });
-  });
+        });
+    });
+
+    app.get('/requester/myevent/:user', function (req, res, next) {
+
+        db.events.findOne({
+          _id: db.ObjectId(req.params.user),
+        }, function (err, data) {
+       	  if(!data)
+       	  	res.send("Param did not work")
+       	  else
+       	  	res.send(JSON.stringify(data));
+        });
+
+    });
+
+
+
 
 };
