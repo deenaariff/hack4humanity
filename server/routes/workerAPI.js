86f3b337badd4da92f6e9d55606c93a5a4f7d834
daariff <aariff.deen@gmail.com>
2016-02-27 15:46:31 -0800

    testing worker APIS

--- server/routes/workerAPI.js
+++ server/routes/workerAPI.js
@@ -1,3 +1,21 @@
-module.exports = function (server, db, ObjectID) {
+module.exports = function (app, db, ObjectID) {
+
+	app.post('/worker/new/:name', function (req, res) {
+        var user_id = ++numworkers;
+        var user_object = {
+        	'id': user_id,
+        	'name': ' ',
+        	'work_info': {
+        		'event': 
+        	},
+        	'req_info': {},
+        	'status': 'available'
+        }
+        db.users.save(user_object,
+          function (err, data) {
+              res.end(JSON.stringify(data));
+          });
+  	});
+
 
 };
