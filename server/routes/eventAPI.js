78081428d399c610e076aa4ec68c2e98a4ecf57a
daariff <aariff.deen@gmail.com>
2016-02-27 18:46:34 -0800

    added events apis

--- server/routes/eventAPI.js
+++ server/routes/eventAPI.js
@@ -1,4 +1,4 @@
-module.exports = function (server, db, ObjectID) {
+module.exports = function (app, db, ObjectID) {
 
 	var ObjectId = ObjectID;
 
@@ -9,9 +9,33 @@ module.exports = function (server, db, ObjectID) {
        	  	res.send("Param did not work")
        	  else
        	  	res.send(JSON.stringify(data));
-        });
+        };
 
     });
+
+	// ADD NEW EVENT
+	// TODO: make sure num users increases in scale of db 
+	app.post('/event/new/:leader', function (req, res) {
+		var data = req.body;
+        var event_object = {
+       		"euid":  db.events.count(),
+     	    "position": {
+     		   "long": data.longit,
+     		   "lat": data.latit
+     	    },
+      		"completed" : 0, 
+     	    "leader": req.params.leader,
+     		"workers_needed": 0,
+     	    "commited_workers": [],
+     	    "potential_workers": [],
+      		"type": data.type,
+      		"severity": data.severity
+        };
+        db.events.save(event_object,
+          function (err, data) {
+              console.log(JSON.stringify(data));
+        });
+  	});
 
 
 
