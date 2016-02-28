13506a41864e7956f1818a25624a03f421eb5dde
Brandon Tsao <BrandonSTsao@gmail.com>
2016-02-27 21:34:55 -0800

    Added removing markers by euid

--- map.js
+++ map.js
@@ -1,5 +1,10 @@
+//globals
+
+var map;
+var markers = {};
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.773972, lng: -122.4167},
     zoom: 13
   });
@@ -7,10 +12,16 @@ function initMap() {
   var infoWindow = new google.maps.InfoWindow({map: map});
 
   //event listener should be replaced with incoming requests
-  document.getElementById("drop-pin").addEventListener('click',function(){addMarker({lat: 37.773972, lng: -122.4167},map,2)});
+  document.getElementById("drop-pin").addEventListener('click',function(){addMarker(0, {lat: 37.773972, lng: -122.4167},map,2)});
 }
 
-function addMarker(location, map, color) {
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function addMarker(euid, location, map, color) {
   //marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
   switch(color){
     case 1:
@@ -27,8 +38,25 @@ function addMarker(location, map, color) {
 
   var marker = new google.maps.Marker({
     //icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+    euid: euid,
     icon: color_icon,
     position: location,
     map: map
   });
+
+  markers[euid] = marker;
+
+  console.log(markers);
+}
+
+function removeMarker(euid){
+  console.log("Removing Marker"+euid);
+  markers[euid].setMap(null);
+
+  // setMapOnAll(null);
+  // delete markers[0];
+  // setMapOnAll(map);
+
+  //console.log("Marker Removed");
+  //markers = markers.filter(function(marker){marker.euid != euid})
 }
