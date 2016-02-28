a6a85dad955c9d1ad1bf03c4472d9397d3df1a63
toemasss <thomasnguyen96@gmail.com>
2016-02-27 20:30:41 -0800

    New User POST test

--- event_form/newUser.js
+++ event_form/newUser.js
@@ -1,18 +1,15 @@
-function submitEvent() {
-	var firstName = document.getElementsByName("name")[0].value;
-
-	console.log("First Name: ", firstName);
-
+function sendAJAX() {
+    var firstName = document.getElementsByName("name")[0].value;
+    console.log("First Name: ", firstName);
 
     var text = '{"firstName": firstName}'
 
-    
 $.ajax({
-  url: "/worker/new/:name"
-  data: text,
-  success: success,
-  dataType: JSON
-});    
+    type: "POST",
+    url: "http://42bc8ac7.ngrok.io/worker/new/" + firstName,
+    success: function(msg){},
+    dataType: JSON
+});
     
-
-}
+} 
+    
